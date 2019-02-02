fce715576064f6f64a378a5008f920c31ee99bf9
zhaoyafei <zhaoyafei0210@gmail.com>
2019-02-03 01:27:00 +0800

    update models/large_margin_module.py

--- models/large_margin_module.py
+++ models/large_margin_module.py
@@ -92,12 +92,15 @@ class LargeMarginModule_Arcface(nn.Module):
 
         self.embedding_net = embedding_net
 
-        self.weight = nn.Parameter(torch.Tensor(
-            self.output_size, self.input_size))
-        nn.init.xavier_uniform_(self.weight)
+        # self.weight = nn.Parameter(torch.Tensor(
+        #     self.output_size, self.input_size))
+        # nn.init.xavier_uniform_(self.weight)
+
+        self.linear = nn.Linear(self.input_size, output_size, bias=False)
+        # self.weight = self.linear.weight
 
     def get_fc_weights(self):
-        wt = self.weight.clone().detach()
+        wt = self.linear.weight.clone().detach()
         return wt
 
     def forward(self, x, targets):
@@ -109,31 +112,34 @@ class LargeMarginModule_Arcface(nn.Module):
         # ebd_norm = torch.norm(embedding, 2, 1)
 
         normalized_ebd = F.normalize(embedding, dim=1)
-        normalized_wt = F.normalize(self.weight, dim=1)
+        normalized_wt = F.normalize(self.linear.weight, dim=1)
 
         cos_theta = F.linear(normalized_ebd, normalized_wt)
-        cos_theta = cos_theta.clamp(-1, 1)
+        # cos_theta = cos_theta.clamp(-1, 1)
 
         # print('---> cos_theta:', cos_theta)
         # print('---> cos_theta.requires_grad:', cos_theta.requires_grad)
 
-        # theta = cos_theta.data.acos()  # no grad here
-        theta = cos_theta.acos()  # theta requires grad here
-        # theta.requires_grad_()
-        # print('---> theta:', theta)
-        # print('---> theta.requires_grad:', theta.requires_grad)
+        if self.m > 0:
+            # theta = cos_theta.data.acos()  # no grad here
+            theta = cos_theta.acos()  # theta requires grad here
+            # theta.requires_grad_()
+            # print('---> theta:', theta)
+            # print('---> theta.requires_grad:', theta.requires_grad)
 
-        # --------------------------- convert targets to one-hot ---------------------------
-        one_hot = torch.zeros_like(cos_theta)
-        one_hot.scatter_(1, targets.view(-1, 1), self.m)
+            # --------------------------- convert targets to one-hot ---------------------------
+            one_hot = torch.zeros_like(cos_theta)
+            one_hot.scatter_(1, targets.view(-1, 1), 1)
 
-        # --------------------------- Calculate output ---------------------------
-        new_theta = theta + one_hot
-        # print('---> theta add one_hot:', new_theta)
-        # print('---> new_theta:', new_theta)
-        # print('---> new_theta.requires_grad:', new_theta.requires_grad)
+            # --------------------------- Calculate output ---------------------------
+            new_theta = theta + one_hot * self.m
+            # print('---> theta add one_hot:', new_theta)
+            # print('---> new_theta:', new_theta)
+            # print('---> new_theta.requires_grad:', new_theta.requires_grad)
 
-        output = new_theta.cos() * self.scale
+            output = new_theta.cos() * self.scale
+        else:
+            output = cos_theta * self.scale
 
         # print('---> output:', output)
         # print('---> output.requires_grad:',
@@ -166,9 +172,10 @@ class LargeMarginModule_ScaledASoftmax(nn.Module):
 
         self.iter = 0
 
-        self.weight = nn.Parameter(torch.Tensor(
-            self.output_size, self.input_size))
-        nn.init.xavier_uniform_(self.weight)
+        # self.weight = nn.Parameter(torch.Tensor(
+        #     self.output_size, self.input_size))
+        # nn.init.xavier_uniform_(self.weight)
+        self.linear = nn.Linear(self.input_size, output_size, bias=False)
 
         # duplication formula
         # refer to:  Double-angle, triple-angle, and half-angle formulae
@@ -183,27 +190,29 @@ class LargeMarginModule_ScaledASoftmax(nn.Module):
         ]
 
     def get_fc_weights(self):
-        wt = self.weight.clone().detach()
+        wt = self.linear.weight.clone().detach()
         return wt
 
     def forward(self, x, targets):
         # print('===> In LargeMarginModule_Arcface.forward()')
 
-        # lambda = max(lambda_min,base*(1+gamma*iteration)^(-power))
-        self.iter += 1
         embedding = self.embedding_net(x)
 
-        self.lamb = max(self.min_lambda, self.base *
-                        (1 + self.gamma * self.iter) ** (-1 * self.power))
+        _lambda = 0
+        if self.min_lambda > 0:
+            # lambda = max(lambda_min,base*(1+gamma*iteration)^(-power))
+            self.iter += 1
+            _lambda = max(self.min_lambda, self.base *
+                          (1 + self.gamma * self.iter) ** (-1 * self.power))
 
         # --------------------------- cos(theta) & phi(theta) ---------------------------
         # ebd_norm = torch.norm(embedding, 2, 1)
 
         normalized_ebd = F.normalize(embedding, dim=1)
-        normalized_wt = F.normalize(self.weight, dim=1)
+        normalized_wt = F.normalize(self.linear.weight, dim=1)
 
         cos_theta = F.linear(normalized_ebd, normalized_wt)
-        cos_theta = cos_theta.clamp(-1, 1)
+        # cos_theta = cos_theta.clamp(-1, 1)
 
         cos_m_theta = self.mlambda[self.m](cos_theta)
         # print('---> cos_theta:', cos_theta)
@@ -224,8 +233,11 @@ class LargeMarginModule_ScaledASoftmax(nn.Module):
         one_hot.scatter_(1, targets.view(-1, 1), 1)
 
         # --------------------------- Calculate output ---------------------------
-        output = (one_hot * (phi_theta - cos_theta) /
-                  (1 + self.lamb)) + cos_theta
+        if _lambda > 0:
+            output = one_hot * (phi_theta - cos_theta) * \
+                (1.0 / (1 + _lambda)) + cos_theta
+        else:
+            output = one_hot * (phi_theta - cos_theta) + cos_theta
 
         # output *= ebd_norm.view(-1, 1)
         output *= self.scale
@@ -258,9 +270,10 @@ class LargeMarginModule_ASoftmaxLoss(nn.Module):
 
         self.iter = 0
 
-        self.weight = nn.Parameter(torch.Tensor(
-            self.output_size, self.input_size))
-        nn.init.xavier_uniform_(self.weight)
+        # self.weight = nn.Parameter(torch.Tensor(
+        #     self.output_size, self.input_size))
+        # nn.init.xavier_uniform_(self.weight)
+        self.linear = nn.Linear(self.input_size, output_size, bias=False)
 
         # duplication formula
         self.mlambda = [
@@ -273,20 +286,28 @@ class LargeMarginModule_ASoftmaxLoss(nn.Module):
         ]
 
     def get_fc_weights(self):
-        wt = self.weight.clone().detach()
+        wt = self.linear.weight.clone().detach()
         return wt
 
     def forward(self, x, targets):
-        # lambda = max(lambda_min,base*(1+gamma*iteration)^(-power))
-        self.iter += 1
         embedding = self.embedding_net(x)
 
-        self.lamb = max(self.min_lambda, self.base *
-                        (1 + self.gamma * self.iter) ** (-1 * self.power))
+        _lambda = 0
+        if self.min_lambda > 0:
+            self.iter += 1
+            # lambda = max(lambda_min,base*(1+gamma*iteration)^(-power))
+            _lambda = max(self.min_lambda, self.base *
+                          (1 + self.gamma * self.iter) ** (-1 * self.power))
 
         # --------------------------- cos(theta) & phi(theta) ---------------------------
-        cos_theta = F.linear(F.normalize(embedding), F.normalize(self.weight))
-        cos_theta = cos_theta.clamp(-1, 1)
+        normalized_ebd = F.normalize(embedding, dim=1)
+        normalized_wt = F.normalize(self.linear.weight, dim=1)
+
+        # cos_theta = F.linear(F.normalize(embedding),
+        #                      F.normalize(self.linear.weight))
+        # cos_theta = cos_theta.clamp(-1, 1)
+        cos_theta = F.linear(normalized_ebd, normalized_wt)
+        # cos_theta = cos_theta.clamp(-1, 1)
 
         cos_m_theta = self.mlambda[self.m](cos_theta)
 
@@ -301,8 +322,12 @@ class LargeMarginModule_ASoftmaxLoss(nn.Module):
         one_hot.scatter_(1, targets.view(-1, 1), 1)
 
         # --------------------------- Calculate output ---------------------------
-        output = (one_hot * (phi_theta - cos_theta) /
-                  (1 + self.lamb)) + cos_theta
+        if _lambda > 0:
+            output = one_hot * (phi_theta - cos_theta) * \
+                (1.0 / (1 + _lambda)) + cos_theta
+        else:
+            output = one_hot * (phi_theta - cos_theta) + cos_theta
+
         output *= ebd_norm.view(-1, 1)
 
         return output
